90a1c683cf31b575fd0ca201ce37af147fc90d08
Didarul Amin <programmer.didarul@gmail.com>
2021-10-31 23:33:30 +0600

    comment

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const cors = require("cors");
 var admin = require("firebase-admin");
 const app = express();
-
+const { v4: uuidv4 } = require("uuid");
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use(express.json());
+const port = process.env.PORT || 5000;
+const dbUrl = process.env.DB_URL;
 
-const { v4: uuidv4 } = require("uuid");
+//firestore setup
 const {
   initializeApp,
   applicationDefault,
@@ -26,23 +28,14 @@ var serviceAccount = require("./tourism-or-delivery-webs-71b76-firebase-adminsdk
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-// console.log(uuidv4());
-
+//firestore initial
 const db = getFirestore();
 
-// console.log(db);
-
-const port = process.env.PORT || 5000;
-const dbUrl = process.env.DB_URL;
-
+//add package
 app.post("/api/addTourPackage/", async (req, res) => {
   const docRef = db.collection("Tour_Packages").doc();
   const data = req.body.data;
   const submitBy = req.body.submitBy;
-  /* console.log(data);
-  console.log(req.body.submitBy); */
-
   await docRef.set({
     id: uuidv4(),
     submitBy,
@@ -52,6 +45,7 @@ app.post("/api/addTourPackage/", async (req, res) => {
   res.send("data added successfully");
 });
 
+//booking api
 app.post("/api/booking/", async (req, res) => {
   const docRef = db.collection("all_bookings").doc();
   const data = req.body.bookingData;
@@ -67,6 +61,8 @@ app.post("/api/booking/", async (req, res) => {
 
   res.send("data added successfully");
 });
+
+//get all package
 
 app.get("/api/allpackages", async (req, res) => {
   const allPackages = [];
@@ -96,6 +92,7 @@ app.get("/api/mybooking/:id", async (req, res) => {
   res.json(mybooking);
 });
 
+// get package by id
 app.get("/api/package/:id", async (req, res) => {
   const Packages = [];
   const Ref = db.collection("Tour_Packages");
@@ -111,6 +108,7 @@ app.get("/api/package/:id", async (req, res) => {
   res.json(Packages);
 });
 
+// get all booking
 app.get("/api/allbookings", async (req, res) => {
   const allbookings = [];
   const citiesRef = db.collection("all_bookings");
@@ -123,6 +121,7 @@ app.get("/api/allbookings", async (req, res) => {
   res.json(allbookings);
 });
 
+// update booking status
 app.put("/api/booking/status/:id", async (req, res) => {
   console.log(req.params.id);
   const Packages = [];
@@ -167,41 +166,6 @@ app.put("/api/package/update/:id", async (req, res) => {
     destination,
     departure_time,
   } = req.body.data;
-  // const Packages = [];
-  /* const Ref = db.collection("Tour_Packages");
-  const snapshot = await Ref.where("id", "==", req.params.id).get();
-  if (snapshot.empty) {
-    console.log("No matching documents.");
-    return;
-  }
-  snapshot.forEach((doc) => {
-    const id = doc.id;
-    Packages.push({ id, ...doc.data() });
-  });
-
-  const up_id = Packages[0].id;
-  console.log(up_id);
-
-  const upRef = db.collection("Tour_Packages").doc(up_id);
-
-  const data = req.body.data;
-
-  const result = await upRef.update({
-    doc_id,
-    not_included,
-    price,
-    departure,
-    img_url,
-    return_time,
-    rating,
-    title,
-    submitBy,
-    tour_duration,
-    accommodation,
-    description,
-    destination,
-    departure_time,
-  }); */
 
   const cityRef = db.collection("Tour_Packages").doc(req.body.data.doc_id);
   const result = await cityRef.update({
@@ -224,6 +188,7 @@ app.put("/api/package/update/:id", async (req, res) => {
   res.json("updatead");
 });
 
+//delete booking
 app.put("/api/booking/delete/:id", async (req, res) => {
   console.log(req.params.id);
   console.log(req.body.status);
@@ -233,6 +198,7 @@ app.put("/api/booking/delete/:id", async (req, res) => {
   res.send(Ref);
 });
 
+//delete package by id
 app.get("/api/package/delete/:id", async (req, res) => {
   console.log(req.params.id);
 
@@ -249,13 +215,10 @@ app.get("/api/package/delete/:id", async (req, res) => {
   res.json(allPackages);
 });
 
+// admin signup
 app.post("/api/signup/admin", async (req, res) => {
   const docRef = db.collection("users").doc();
   const data = req.body.data;
-  // console.log(data);
-  // const submitBy = req.body.submitBy;
-  /* console.log(data);
-  console.log(req.body.submitBy); */
 
   await docRef.set({
     userId: uuidv4(),
@@ -264,6 +227,7 @@ app.post("/api/signup/admin", async (req, res) => {
   res.send("Request has sent");
 });
 
+//check admin permissions
 app.get("/api/signup/admin/check/:email", async (req, res) => {
   const Packages = [];
   const Ref = db.collection("users");
@@ -280,10 +244,12 @@ app.get("/api/signup/admin/check/:email", async (req, res) => {
   res.json(Packages[0]);
 });
 
+//r server running messages
 app.get("/", async (req, res) => {
   res.send("server is listening");
 });
 
+//server listening on
 app.listen(port, () => {
   console.log("listening on port", port);
 });
